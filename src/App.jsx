7c762b4468f272d3e76fa118a2acd437265e7e97
Vladislav <vladislavprokhorov1005@gmail.com>
2025-10-24 19:48:11 +0300

    Created button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Title from './Components/Title/Title'
+import Button from './Components/Button/Button'
 
 export default function App() {
   return (
     <div className='container'>
       <Title text={"Форма пошуку турів"} />
+      <Button />
     </div>
   )
 }
--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import './Button.css'
 
 export default function Button() {
   return (
-    <div>Button</div>
+    <button className='btn'>Знайти</button>
   )
 }
